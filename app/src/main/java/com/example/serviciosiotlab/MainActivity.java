a07102277f289f0fc4775148498ae7bed722890d
Joau0824 <a20182162@pucp.edu.pe>
2022-09-04 19:29:45 -0500

    Mis cosas

--- app/src/main/java/com/example/serviciosiotlab/MainActivity.java
+++ app/src/main/java/com/example/serviciosiotlab/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.serviciosiotlab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_inicio);
+
+        Button button =findViewById(R.id.btn_memoria);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
+
+
+
 }
