4dc10b79958acbbb986254081590a323ed53b280
Stephania <stephania.guzman@pucp.edu.pe>
2022-09-04 22:35:22 -0500

    pocas otras cosas

--- app/src/main/java/com/example/serviciosiotlab/ActivityMemoriaEstadisticas.java
+++ app/src/main/java/com/example/serviciosiotlab/ActivityMemoriaEstadisticas.java
@@ -1,4 +1,38 @@
 package com.example.serviciosiotlab;
 
-public class ActivityMemoriaEstadisticas {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ActivityMemoriaEstadisticas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.memoria_estadisticas);
+        getSupportActionBar().setTitle("Juego - Memoria");
+
+        Intent intent = getIntent();
+        ArrayList<String> estadisticas = intent.getStringArrayListExtra("estadisticas");
+        if (estadisticas != null && !estadisticas.isEmpty()) {
+            String estadisticasStr = "";
+            for (String i : estadisticas) {
+                estadisticasStr = estadisticasStr + i + '\n';
+            }
+            TextView textView = findViewById(R.id.textStatsTic);
+            textView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            textView.setText(estadisticasStr);
+        }
+    }
+
+    public void nuevoJuego(View view) {
+        Intent intent = new Intent(this, MainActivityMemoria.class);
+        intent.putExtra("nuevoJuego", 1);
+        startActivity(intent);
+    }
+
 }
